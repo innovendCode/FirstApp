ff0dd5638a3b0275acd1a2e3841801e81d060162
innovendCode <mgchibuye@gmail.com>
2020-07-07 12:31:44 +0200

    Navigation drawer & Fragments

---
+++ app/src/main/java/com/example/sonka/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sonka;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+NavigationView navigationView;
+Toolbar toolbar;
+DrawerLayout drawerLayout;
+ActionBarDrawerToggle actionBarDrawerToggle;
+FragmentManager fragmentManager;
+FragmentTransaction fragmentTransaction;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        setSupportActionBar(toolbar);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, new MainFragment());
+        fragmentTransaction.commit();
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+    drawerLayout.closeDrawer(GravityCompat.START);
+        if (item.getItemId() == R.id.home){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new MainFragment());
+            fragmentTransaction.commit();
+        }
+
+        if (item.getItemId() == R.id.members){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, new MembersFragment());
+            fragmentTransaction.commit();
+        }
+
+
+        return false;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sonka/MembersFragment.java
@@ -0,0 +1,32 @@
+package com.example.sonka;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class MembersFragment extends Fragment {
+FloatingActionButton btn_addMember;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_members, container, false);
+        return view;
+
+
+    }
+
+
+
+
+}
